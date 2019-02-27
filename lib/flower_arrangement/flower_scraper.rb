4fb2b9fe4ae5cada617f315197a4856b230dc031
A Glazer <avigayilglazer@gmail.com>
2019-02-27 20:32:23 +0000

    added the first method to scrape main website

---
+++ lib/flower_arrangement/flower_scraper.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class FlowerArrangement::FlowerScraper
+  
+  def self.scrape_main_website
+    doc = Nokogiri::HTML(open("https://modernflorist.com/"))
+    binding.pry
+    
+  end
+  
+    # def self.scrape_woot
+    # doc = Nokogiri::HTML(open("https://woot.com"))
+
+    # deal = self.new
+    # deal.name = doc.search("h2.main-title").text.strip
+    # deal.price = doc.search("#todays-deal span.price").text.strip
+    # deal.url = doc.search("a.wantone").first.attr("href").strip
+    # deal.availability = true
+
+    # deal
+  # end
+  
+  
+end
+
+FlowerScraper.scrape_main_website
