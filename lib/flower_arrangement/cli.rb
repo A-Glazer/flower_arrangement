e9bea08796983ef973767fb8d9c5fad34c887d7e
A Glazer <avigayilglazer@gmail.com>
2019-03-03 18:43:42 +0000

    working on scraping errors

--- lib/flower_arrangement/cli.rb
+++ lib/flower_arrangement/cli.rb
@@ -1,10 +1,41 @@
 require_relative 'version.rb' #remove before saving final project
+# FlowerArrangement::FlowerScraper.scrape_main_website #test with bin/controller - running like user
 class FlowerArrangement::CLI
+  attr_accessor :name, :price, :url
 
+  def initialize
+    @name = name
+    @price = price
+    @url = url
+  end
+  
+  
   def call
-    puts "Testing 123"
-    FlowerArrangement::FlowerScraper.scrape_main_website #test with bin/controller - running like user
+    greeting
+    
+  end
+  
+  def greeting
+    puts "Welcome to Modern Florist, please enter a price range from the choices below:"
+    puts "1. $45 - $79"
+    puts "2. $80 - $149"
+    puts "3. $150 and up"
   end
 
+  def user_input_greeting
+    input = gets.strip
+    
+    if input == "1"
+      puts "name"
+      
+    end 
+    
+  end
+  
+  def scraper_by_category
+    FlowerArrangement::FlowerScraper.scrape_main_website
+    binding.pry
+  end
+  
 end
 
--- lib/flower_arrangement/flower_scraper.rb
+++ lib/flower_arrangement/flower_scraper.rb
@@ -1,31 +1,41 @@
 require_relative 'version.rb'
+
 class FlowerArrangement::FlowerScraper
-  # attr_accessor :name, :price, :url => move to flower.rb
+  attr_accessor :name, :price, :url 
+  @@all = []
+
+
+  def initialize(name, price, url)
+    @name = name 
+    @price = price 
+    @url = url
+    @@all << self
+  end 
   
-  # need to initialize a flower object (name, price, url)
-  # @name = name
+  
+  def self.all 
+    @@all  
+  end 
+  
+  
   
   def self.scrape_main_website
     doc = Nokogiri::HTML(open("https://modernflorist.com/"))
-    name = doc.css("a.hover-info span.product_title")
-      name.collect{|title| title.text}
+    
+    name = doc.css("a.hover-info span.product_title")[0].text
+      # name.collect{|title| title.text}
       
     price = doc.css("a.hover-info span.price").text.split("$")
       price.collect {|amount| "${amount}"}.shift
       
     url = doc.css("a.more-link").attr("href").value
+    
+    flower = self.new(name, price, url) 
+    
+    @@all << flower
     binding.pry
-      
-      
+
   end
   
 end
 
-#doc =>
-#step 1. scrape everything
-#step 2. create flower.rb file and class FlowerArrangement::Flower
-#     in flower class initialize with name, price, url
-# (initialize is ready to accept orders)
-
-# [array 123924394 @name = FlowerArrangement::Flower.new(name, price, url), @url = url, @price = price]
-#CLI will call on the scraper and will pass in the name, price and url argument
