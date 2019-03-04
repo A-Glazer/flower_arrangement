6b67d370729b04d34857e74e258d5eb18e76fa9d
A Glazer <avigayilglazer@gmail.com>
2019-03-04 11:04:46 +0000

    scrape_main_website works (number of flowers is hard coded)

--- lib/flower_arrangement/flower_scraper.rb
+++ lib/flower_arrangement/flower_scraper.rb
@@ -5,7 +5,6 @@ class FlowerArrangement::FlowerScraper
   @@flower_array = []
 
   def self.flower_array
-    
     flower_array << self.scrape_main_website
     binding.pry
   end
@@ -15,19 +14,37 @@ class FlowerArrangement::FlowerScraper
   def self.scrape_main_website
     doc = Nokogiri::HTML(open("https://modernflorist.com/"))
     
-    i =  1
-    while i < 16
+    i = 0
+    while i < 15
       
     flower = self.new
-    flower.name = doc.css("a.hover-info span.product_title")[0].text
-    flower.price = doc.css("a.hover-info span.price")[0].text
-    flower.url = doc.css("a.more-link").attr("href").value
+    flower.name = doc.css("a.hover-info span.product_title")[i].text
+    flower.price = doc.css("a.hover-info span.price")[i].text
+    flower.url = doc.css("a.more-link")[i].attr("href")
+    @@flower_array << flower
     i += 1 
   end 
-  @@flower_array
-    binding.pry
-
+  @@flower_array 
   end
   
 end
 
+
+
+# def self.scrape_main_website
+#     doc = Nokogiri::HTML(open("https://modernflorist.com/"))
+    
+#     i =  1
+#     while i < 16
+      
+#     flower = self.new
+#     flower.name = doc.css("a.hover-info span.product_title")[0].text
+#     flower.price = doc.css("a.hover-info span.price")[0].text
+#     flower.url = doc.css("a.more-link").attr("href").value
+#     @@flower_array << flower
+#     i += 1 
+#   end 
+#   @@flower_array 
+#     binding.pry
+
+#   end
